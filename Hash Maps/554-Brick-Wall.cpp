517f36172d67b5de97de9d976cc296b78aa060ee
vivekarora2000 <vivekarora2000@gmail.com>
2024-02-26 08:53:34 -0500

    554

---
+++ Hash Maps/554-Brick-Wall.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        
+        // Key is the position and Value is count of corners
+        unordered_map<int, int> num_corners;
+
+        int pos, max_count = 0;
+
+        for(int i = 0; i < wall.size(); i++){
+            pos = 0;
+            for(int j = 0; j < wall[i].size() - 1; j++){
+
+                // Calculate position using width
+                pos += wall[i][j];
+
+                num_corners[pos] += 1;
+
+                max_count = max(max_count, num_corners[pos]);
+            }
+        }
+
+        return wall.size() - max_count;
+
+    }
+};
